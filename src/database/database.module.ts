1e5bfca0fb06fe4f8e7bde9b23729ea7fa2ccd2b
adis.sun <adis.sun@innocellence.com>
2020-05-29 09:53:44 +0800

    feat(database): add database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,34 @@
+import { DynamicModule, Global, Module, OnModuleInit } from '@nestjs/common';
+import { InjectConnection, TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import _ from 'lodash';
+import { Connection } from 'typeorm';
+
+@Global()
+@Module({})
+export class DataBaseModule implements OnModuleInit {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  public static forRoot(options: TypeOrmModuleOptions): DynamicModule {
+    const modules = [TypeOrmModule.forRoot(options)];
+    return {
+      module: DataBaseModule,
+      imports: [...modules],
+    };
+  }
+
+  public async onModuleInit(): Promise<void> {
+    const databaseSynchronize = process.env.DATABASE_SYNCHRONIZE === 'true';
+    if (databaseSynchronize) {
+      const { driver, entityMetadatas } = this.connection;
+      const queryRunner = driver.createQueryRunner('master');
+      const databaseSchemas = await queryRunner.getSchemas();
+      const schemas = _.chain(entityMetadatas)
+        .map('schema')
+        .compact()
+        .difference(databaseSchemas)
+        .value();
+      await Promise.all(schemas.map((schema) => queryRunner.createSchema(schema)));
+      await this.connection.synchronize();
+    }
+  }
+}
