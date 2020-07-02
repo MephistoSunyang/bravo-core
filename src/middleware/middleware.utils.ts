57c23deb68138bfffd3725b53affede33b06f814
adis.sun <adis.sun@innocellence.com>
2020-07-02 14:31:26 +0800

    feat(middleware): add middleware method

--- src/middleware/middleware.utils.ts
+++ src/middleware/middleware.utils.ts
@@ -1,6 +1,10 @@
 import httpContext from 'express-http-context';
 import { IObject } from '../shared';
 
+export function setCurrentUserId(userId: string) {
+  httpContext.set('user', { id: userId });
+}
+
 export function getCurrentUser<IData = IObject>(field?: string): IData | null {
   const user = httpContext.get('user') as IData;
   return user ? (field ? user[field] : user) : null;
